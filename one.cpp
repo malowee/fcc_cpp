3659623b459426ba65498389d3e41cc07890dd24
malowee <ilemona.atawodi@usm.edu>
2020-05-21 02:35:27 -0500

    new

--- one.cpp
+++ one.cpp
@@ -2,15 +2,17 @@
 #include <cmath>
 
 //Day: 2, May 21 2020
+//
 using namespace std;
 
 int main() 
 {
-    //first thing i'm practicing is git
-    cout<<"Hello, World"<<endl;
-    cout<<pow(45, 2)<<endl;
-    return 0;
-    //what if we return back to mom or not.?
+    string colour, noun, celebrity;
+    cout<< " Enter a Colour: "<<endl;
+    getline(cin, colour);
+
+    cout<<colour<<endl;    
+
 }
 
 
