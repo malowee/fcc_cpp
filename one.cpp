2bcabb8a6ff3afcb25af16c80a9ee6dbd35dc361
malowee <ilemona.atawodi@usm.edu>
2020-05-21 02:12:08 -0500

    cmath test

--- one.cpp
+++ one.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <cmath>
 
+//Day: 2, May 21 2020
 using namespace std;
 
 int main() 
 {
     //first thing i'm practicing is git
     cout<<"Hello, World"<<endl;
-    cout<<"create olive branch"<<endl;
+    cout<<pow(45, 2)<<endl;
     return 0;
     //what if we return back to mom or not.?
 }
