ba24f676cdb80b9941545349e37f76dac7a2f27f
malowee <ilemona.atawodi@usm.edu>
2020-05-25 02:13:57 -0500

    thinking

--- one.cpp
+++ one.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <cmath>
 
-//Day: 2, May 21 2020
-//
+//Day: 3, May 25 2020
+//Time to find a project i think
+
+
 using namespace std;
 
 int main() 
 {
-    string colour, noun, celebrity;
-    cout<< " Enter a Colour: "<<endl;
-    getline(cin, colour);
+    //Arrays
+    //int newArray[20] = {1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1,0};
+    //newArray[3] = 12;
+    //cout<<newArray[3];
 
-    cout<<colour<<endl;    
+    //functions
+
 
 }
 
